413a5a79d338dca5992acbafaa2b7d7f3f5e7052
kewiibird <kewiibird@gmail.com>
2026-04-20 18:00:45 +0900

    feat : product-list 필터링 , detail 이미지 불러오기 ...

--- modak/src/main/java/com/example/modak/product/model/Product.java
+++ modak/src/main/java/com/example/modak/product/model/Product.java
@@ -15,9 +15,12 @@ public class Product {
 	private String createdAt; // 등록일
 	
 	private int categoryId; // 카테고리id
+	private String categoryName; // 카테고리name
 	
-	private String imgUrl; // ⭐ 대표 이미지 추가
-	
+	private String imgUrl; // 제품 사진
+	private String mainImg; // ⭐ IS_MAIN 값 (Y/N) 을 담을 필드 추가
+    private int sortOrder; // ⭐ 순서 값을 담을 필드 추가
+    
 	private int brandId; // 브랜드 아이디 1,2,3
 	private String brandName; // 브랜드명 - 브랜드없으면 1번 자체제작
 }

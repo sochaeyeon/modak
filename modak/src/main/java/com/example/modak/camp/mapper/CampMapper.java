6cf983c0532861472bfedd8357ce41a7eb6e1911
eundong <rladmsehd135@gmail.com>
2026-04-21 09:57:28 +0900

    fix/게스트 주문 상세 조회 css분리 fix/캠핑장 찾기 수정중

--- modak/src/main/java/com/example/modak/camp/mapper/CampMapper.java
+++ modak/src/main/java/com/example/modak/camp/mapper/CampMapper.java
@@ -9,18 +9,19 @@ import com.example.modak.camp.model.Camp;
 
 @Mapper
 public interface CampMapper {
-    // 캠핑장 목록 조회
+
+    /** 캠핑장 목록 조회 (중복 제거 DB 처리) */
     List<Camp> selectCampList(HashMap<String, Object> params);
 
-    // CAMP 테이블 저장
+    /** CAMP 테이블 저장 */
     int insertCampList(List<Camp> list);
 
-    // CAMP_IMG 테이블 저장 (추가됨)
+    /** CAMP_IMG 테이블 저장 */
     int insertCampImg(HashMap<String, Object> params);
 
-    // 리뷰 목록 조회
+    /** 리뷰 목록 조회 */
     List<HashMap<String, Object>> selectReviewList(HashMap<String, Object> params);
 
-    // 리뷰 등록
+    /** 리뷰 등록 */
     int insertReview(HashMap<String, Object> params);
 }
